4c0fa19e0fb1347077add66b32a32df82fcea24d
Rajeshwari <rajeshwarirajeshwari34879@gmail.com>
2026-04-29 09:53:39 +0530

    Modified the Jd code

--- Operators.js
+++ Operators.js
@@ -3,149 +3,149 @@
 
 // 1. Arithmetic Operator  => Arithmetic operation are used to perform  mathematical operation on operand
 
-console.log("----------------Arithmetic Operator-------------------")
+console.log("----------------Arithmetic Operator-------------------");
 
 // a. Addition  
 
 let a = 10;
 let b = 20;
 let c = a+b;
-console.log("Addition of two numbers",c);
+console.log("Addition of two numbers",c);;
 
 // b. subtraction
 
 let d = a-b;
-console.log("subtraction of two numbers", d);
+console.log("subtraction of two numbers", d);;
 
 // c. Multiplication
 
 let e = a*b;
-console.log("Multiplication two numbers", e)
+console.log("Multiplication two numbers", e);
 
 
 //d. division
 let f = a/b;
-console.log("division two numbers", f);
+console.log("division two numbers", f);;
 
 //e. mudulo
 
 let g = a%b;
-console.log("mudulo two numbers", g);
+console.log("mudulo two numbers", g);;
 
 //f. Exponentiation
 
 let h = a**b;
-console.log("Multiplication two numbers", h)   // 10^20
+console.log("Multiplication two numbers", h);   // 10^20
 
-// 2. Assignment Operation(=)  => Its used to assign the value to variable
+// 2. Assignment Operation(=);  => Its used to assign the value to variable
 
-console.log("------------------Assignment Operation-------------------")
+console.log("------------------Assignment Operation-------------------");
 
 let x = 5;
 
 x += 3  // x = x+3 => x = 5+3
-console.log(x)
+console.log(x);
 
 x -= 3  // x = x-3 => x = 5-3
-console.log(x)
+console.log(x);
 
 x *= 3  // x = x*3 => x = 5*3
-console.log(x)
+console.log(x);
 
 x /= 3  // x = x/3 => x = 5/3
-console.log(x)
+console.log(x);
 
 x **= 3  // x = x**3 => x = 5+**3
-console.log(x)
+console.log(x);
 
-// 3. Increment (++)/ Decrement(--) Operators  => the Increment and decrement operator used to increase or decrease the value of a variable 1.
+// 3. Increment (++);/ Decrement(--); Operators  => the Increment and decrement operator used to increase or decrease the value of a variable 1.
 
-console.log("----------------3. Increment (++)/ Decrement(--)-----------------")
+console.log("----------------3. Increment (++);/ Decrement(--);-----------------");
 
 // there are used two ways prefix and postfix
-// 1. Prefix(++i)  => the value of variable is increased before it is used
-console.log("----prefix (++i)----")
+// 1. Prefix(++i);  => the value of variable is increased before it is used
+console.log("----prefix (++i);----");
 
 let y = 10;
-console.log("increment the elements before the operands",++y)
-console.log(y)
-
-
-console.log("decrements the elements before the operands",--y)
-console.log(y)
-
-console.log("----Postfix (i++)----")
-
-console.log("increment the elements after the operands",y++)
-console.log(y)
-
-
-console.log("decrements the elements after the operands",y--)
+console.log("increment the elements before the operands",++y);
 console.log(y);
+
+
+console.log("decrements the elements before the operands",--y);
+console.log(y);
+
+console.log("----Postfix (i++);----");
+
+console.log("increment the elements after the operands",y++);
+console.log(y);
+
+
+console.log("decrements the elements after the operands",y--);
+console.log(y);;
 
 // 4. comparison operator => its compare two values and give back a boolean value: true or false
 //its useful in decision-making and looping condition is javascript
 
 
-console.log("------------------- comparison operator -----------------------")
-// <(less than)
-// >(grater than)
-// =(equal to)
-// <=(less than eqaul to)
-// >=(grater than  equal to)
+console.log("------------------- comparison operator -----------------------");
+// <(less than);
+// >(grater than);
+// =(equal to);
+// <=(less than eqaul to);
+// >=(grater than  equal to);
 
-// ==(equal check)
-// !=(not equal or inequal)
-// ===(strict equality checking) (check tha data types and number) 
-// !==(strick inequality) (check the datatype)
+// ==(equal check);
+// !=(not equal or inequal);
+// ===(strict equality checking); (check tha data types and number); 
+// !==(strick inequality); (check the datatype);
 
 
 let p = 10
 let q = 20
 
-console.log(p < q);  // true
-console.log(p > q);  // false
-console.log(p <= q);  // true
-console.log(p >= q);  // false 
+console.log(p < q);;  // true
+console.log(p > q);;  // false
+console.log(p <= q);;  // true
+console.log(p >= q);;  // false 
 
-console.log(p == q);  // false
-console.log(p != q);  // true
+console.log(p == q);;  // false
+console.log(p != q);;  // true
 
 let r = '10'
-console.log(p === q);  // false
-console.log(p !== q);  // true
+console.log(p === q);;  // false
+console.log(p !== q);;  // true
 
-console.log(p === r);  // false
-console.log(p !== r);  // true
+console.log(p === r);;  // false
+console.log(p !== r);;  // true
 
 //  5. logical operator => logical operator perform the logically
 
-console.log("-----------------logical Operator--------------------");
+console.log("-----------------logical Operator--------------------");;
 
-// AND (&&),
-// OR (||),
-// NOT (!)
+// AND (&&);,
+// OR (||);,
+// NOT (!);
 
-// a. AND(&&) => its true is all condition is true
+// a. AND(&&); => its true is all condition is true
 
 // true && true // true
 // true && false // false
 // false && true // false
 // false && false // false
 
-console.log("---AND(&&) when all condition is true then its true---")
+console.log("---AND(&&); when all condition is true then its true---");
 
 let s = 10;
 let t = 20;
 
-console.log(s > 0 && t > 0);  // true
-console.log(s < 0 && t > 0);  // false
-console.log(s > 0 && t < 0);  // false
-console.log(s < 0 && t < 0);  // false
+console.log(s > 0 && t > 0);;  // true
+console.log(s < 0 && t > 0);;  // false
+console.log(s > 0 && t < 0);;  // false
+console.log(s < 0 && t < 0);;  // false
 
-// b. OR (||) => its true when at least one condition is true
+// b. OR (||); => its true when at least one condition is true
 
-console.log("----OR (||) => its true when at least one condition is true ----")
+console.log("----OR (||); => its true when at least one condition is true ----");
 
 // true || false // true
 // false || true // true
@@ -155,37 +155,37 @@ console.log("----OR (||) => its true when at least one condition is true ----")
 let m = 10
 let n = 5;
 
-console.log(m > 0 || n < 0) // true
-console.log(m < 0 || n > 0) // true
-console.log(m > 0 || n > 0) // true
-console.log(m < 0 || n < 0) // false
+console.log(m > 0 || n < 0); // true
+console.log(m < 0 || n > 0); // true
+console.log(m > 0 || n > 0); // true
+console.log(m < 0 || n < 0); // false
 
-// c. NOT (!) => when condition is true then its false and vise versa
+// c. NOT (!); => when condition is true then its false and vise versa
 
 // !true // false
 // !false // true
 
-console.log("----- NOT (!) => when condition is true then its false and vise versa---- ");
+console.log("----- NOT (!); => when condition is true then its false and vise versa---- ");;
 
 let Yes = true;
 let No = false
-console.log(!Yes) // false
-console.log(!No) // true
+console.log(!Yes); // false
+console.log(!No); // true
 
 
-// 6. String Operator => we can also use the operator to concatenate (join) two or more string
+// 6. String Operator => we can also use the operator to concatenate (join); two or more string
 
-console.log("-------------------String Operator => its add the two or more strings ----")
+console.log("-------------------String Operator => its add the two or more strings ----");
 
-console.log("Aishu and " + "Raji" + " are friends")
+console.log("Aishu and " + "Raji" + " are friends");
 
 let nam1 = "Keerthana";
 let nam2 = "Child";
-console.log(nam1 + " is a " + nam2)
+console.log(nam1 + " is a " + nam2);
 
 let num ="Hello Welcome to";
 num += " Javascript World"
-console.log(num)
+console.log(num);
 
 
 
