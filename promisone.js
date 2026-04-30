7538777970386850b7a5b327fa4975e4d3380dac
Rajeshwari <rajeshwarirajeshwari34879@gmail.com>
2026-04-30 11:32:34 +0530

    Js validateing using promisone

--- promisone.js
+++ promisone.js
@@ -5,21 +5,21 @@
 // reject => task is rejected
 
 function fetchData(){
-    fetch('https://fakestoreapi.com/products')  // load call api  // pending state
+    fetch('https://fakestoreapi.com/products');  // load call api  // pending state
 
     .then(response=>{          // producing code
-        if(!response.ok)   // not the call api throw the error
+        if(!response.ok);   // not the call api throw the error
         {
-            throw new Error("networking error 404");    // incase any error happen while api loading throw the one error
+            throw new Error("networking error 404");;    // incase any error happen while api loading throw the one error
         }
         return response.json();  // if task is successful then give the data in json format
-    })
-    .then(data=>{        // consuming code     // fulfill state => task is successful
-        console.log(data)  // here display the data in storing the data in json format
-    })
-    .catch(error=>{           // rejected state => task is rejected
-        console.error("show the error here",error);   // error given the error
     });
+    .then(data=>{        // consuming code     // fulfill state => task is successful
+        console.log(data);  // here display the data in storing the data in json format
+    });
+    .catch(error=>{           // rejected state => task is rejected
+        console.error("show the error here",error);;   // error given the error
+    });;
 
 }
 
@@ -29,20 +29,20 @@ fetchData();   // call the function name here
 
 function cardData(){
 
-    fetch('https://fakestoreapi.com/carts')   // load  call api  => pending state
+    fetch('https://fakestoreapi.com/carts');   // load  call api  => pending state
 
     .then(response=>{            // check the api is success or rejected
-        if(!response.ok)
+        if(!response.ok);
         {
-            throw new Error("networking error 404");   //when api is loaded any error coming this executed 
+            throw new Error("networking error 404");;   //when api is loaded any error coming this executed 
         }
         return response.json();   //  api loaded successful then give the data in json() format
-    })
+    });
     .then(data=>{           // fulfilled state
-        console.log(data)    // here json() formate data display
-    })
+        console.log(data);    // here json() formate data display
+    });
     .catch(error=>{    // rejected state 
-        console.error("Something Error is occuping",error);   // error showing
-    })
+        console.error("Something Error is occuping",error);;   // error showing
+    });
 }
 cardData();
